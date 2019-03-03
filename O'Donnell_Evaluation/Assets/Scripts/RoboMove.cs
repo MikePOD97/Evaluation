57b412d8c3b24227cadccbc53472f62f05392a3f
Mike O'Donnell <mxo7550@rit.edu>
2019-03-03 00:25:02 -0500

    Created Project, added basic movement.

---
+++ O'Donnell_Evaluation/Assets/Scripts/RoboMove.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoboMove : MonoBehaviour {
+
+    // Use this for initialization
+    Vector3 pos;
+    Vector3 speed;
+	void Start ()
+    {
+        pos = new Vector3(0, 0, 0);
+        speed = new Vector3(0.1f, 0, 0);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKey(KeyCode.D))
+            pos.x += speed.x;
+        if (Input.GetKey(KeyCode.A))
+            pos.x -= speed.x;
+        transform.position = pos;
+	}
+}
