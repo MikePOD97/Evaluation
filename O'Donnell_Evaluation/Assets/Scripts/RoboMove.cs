a0f2ba4d9223edcb5ef69a0bb1c886b32664633b
Mike O'Donnell <mxo7550@rit.edu>
2019-03-03 02:54:37 -0500

    Added walking animations, need tweaking.

--- O'Donnell_Evaluation/Assets/Scripts/RoboMove.cs
+++ O'Donnell_Evaluation/Assets/Scripts/RoboMove.cs
@@ -5,21 +5,51 @@ using UnityEngine;
 public class RoboMove : MonoBehaviour {
 
     // Use this for initialization
-    Vector3 pos;
-    Vector3 speed;
+    //Variables to freely change the robot's position and speed
+    private Vector3 pos;
+    private Vector3 speed;
+    private Animator animator;
+    private bool isIdle = true;
+    private bool isWalking = false;
 	void Start ()
     {
+        //Initialize them
         pos = new Vector3(0, 0, 0);
-        speed = new Vector3(0.1f, 0, 0);
-	}
+        speed = new Vector3(2f, 0, 0);
+        animator = GameObject.Find("PlayerRobot").GetComponent<Animator>();
+    }
 	
 	// Update is called once per frame
 	void Update ()
     {
         if (Input.GetKey(KeyCode.D))
-            pos.x += speed.x;
-        if (Input.GetKey(KeyCode.A))
-            pos.x -= speed.x;
-        transform.position = pos;
-	}
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * speed.x);
+            if(!isWalking)
+            {
+                isWalking = true;
+                isIdle = false;
+               
+            }
+        }
+           
+        else if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * (-1 * speed.x));
+            if (!isWalking)
+            {
+                isWalking = true;
+                isIdle = false;
+               
+            }
+        }
+        else
+        {
+            isWalking = false;
+            isIdle = true;
+          
+        }
+        animator.SetBool("isIdle", isIdle);
+        animator.SetBool("isWalking", isWalking);
+    }
 }
